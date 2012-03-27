b00a65bfb9f92edd6092dc5250a1c79f8c12507d
Scott M. Barbour <smbarbour@gmail.com>
2012-03-27 01:33:18 -0500

    Some updates... too tired to detail

--- src/cah/melonar/ChunkyBlocks.java
+++ src/cah/melonar/ChunkyBlocks.java
@@ -16,34 +16,53 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class ChunkyBlocks extends JavaPlugin implements Listener {
 	private final Logger logger = Logger.getLogger("Minecraft");
 	private PluginManager pm;
+	private FileConfiguration myConfig = this.getConfig();
+	private boolean useBlock;
+	private int loadRadius;
+	private int minHeight;
+	private int maxHeight;
+	private int maxChunks;
+	private Material clMaterial;
 
 	@Override
 	public void onDisable(){
 		PluginDescriptionFile pdfFile = this.getDescription();
 		logger.info(pdfFile.getName() + " is now disabled.");
 	}
-	
+
 	@Override
 	public void onEnable(){
 		pm = getServer().getPluginManager();
 		pm.registerEvents(this, this);
 		PluginDescriptionFile pdfFile = this.getDescription();
 		logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled.");
-		FileConfiguration myConfig = this.getConfig();
+		loadConfig();
+	}
+
+	private void loadConfig(){
+		useBlock = myConfig.getBoolean("useBlock", true);
+		loadRadius = myConfig.getInt("radius", 1);
+		minHeight = myConfig.getInt("minHeight",54);
+		maxHeight = myConfig.getInt("maxHeight",74);
+		clMaterial = Material.getMaterial(myConfig.getInt("blockType", 19));
+		maxChunks = myConfig.getInt("maxChunksPerUser",1);
+
 	}
 
 	@EventHandler
 	public final void cbChunkUnload(ChunkUnloadEvent cuEvent){
 		World currentWorld = cuEvent.getWorld();
-		for (int worldX = -3; worldX <= 3; worldX++){
-			for (int worldZ = -3; worldZ <= 3; worldZ++){
-				Chunk currentChunk = currentWorld.getChunkAt(cuEvent.getChunk().getX()+worldX, cuEvent.getChunk().getZ()+worldZ);
-				for (int chunkX = 0; chunkX <= 15; chunkX++){
-					for (int chunkZ = 0; chunkZ <= 15; chunkZ++){
-						for (int chunkY = 54; chunkY <= 74; chunkY++){
-							if (currentChunk.getBlock(chunkX, chunkY, chunkZ).getType() == Material.SPONGE){
-								cuEvent.setCancelled(true);
-								break;
+		if(useBlock==true){
+			for (int worldX = (-1 * loadRadius); worldX <= loadRadius; worldX++){
+				for (int worldZ = (-1 * loadRadius); worldZ <= loadRadius; worldZ++){
+					Chunk currentChunk = currentWorld.getChunkAt(cuEvent.getChunk().getX()+worldX, cuEvent.getChunk().getZ()+worldZ);
+					for (int chunkX = 0; chunkX <= 15; chunkX++){
+						for (int chunkZ = 0; chunkZ <= 15; chunkZ++){
+							for (int chunkY = minHeight; chunkY <= maxHeight; chunkY++){
+								if (currentChunk.getBlock(chunkX, chunkY, chunkZ).getType() == clMaterial){
+									cuEvent.setCancelled(true);
+									break;
+								}
 							}
 						}
 					}
